2914bcd9a08e19a621da7f59928603d40364a832
梅鹏 <1148017085@qq.com>
2018-11-12 15:26:27 +0800

    Revert "hello world"
    This reverts commit f7f0adab34f0c256c09c2ac09c05431d4365558f.

--- app/src/main/java/dianshang/com/shopping/MainActivity.kt
+++ app/src/main/java/dianshang/com/shopping/MainActivity.kt
@@ -2,14 +2,11 @@ package dianshang.com.shopping
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Toast.makeText(this,"hello world",Toast.LENGTH_LONG).show()
     }
 }
